148dce7eb7cd88c7c3ae87c7094566583d6086a2
iriring <juju5064@naver.com>
2025-11-11 22:27:07 +0900

    캐릭터 움직임 대충 구현

--- character.py
+++ character.py
@@ -1,13 +1,67 @@
-from pico2d import load_image
+from pico2d import *
+
+open_canvas()
+character = load_image('arang.png')
+frame = 0
+running = True
+x = 400
+y=120
+dir = 0
+updown = 0
+def handle_events():
+    global running, dir, updown
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_UP:
+                updown += 1
+            elif event.key == SDLK_DOWN:
+                updown -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                    dir -= 1
+            elif event.key == SDLK_LEFT:
+                    dir += 1
+            elif event.key == SDLK_UP:
+                updown -= 1
+            elif event.key == SDLK_DOWN:
+                updown += 1
 
 
-class arang:
-    def __init__ (self):
-        self.x, self.y = 16, 32
-        self.frame = 4
-        self.face_dir = 1
-        self.image = load_image('arang.png')
+while running:
+    clear_canvas()
+    handle_events()
+    if updown == 1:
+        character.clip_draw(frame * 16, 32, 16, 32, x, y, 100, 200)
+    elif updown == -1:
+        character.clip_draw(frame * 16, 96, 16, 32, x, y, 100, 200)
+    elif dir == 1:
+        character.clip_draw(frame * 16, 64, 16, 32, x, y, 100, 200)
+    elif dir == -1:  ㅋ
+        character.clip_draw(frame * 16, 0, 16, 32, x, y, 100, 200)
+    else:
+        if updown == 1:
+            character.clip_draw(0, 32, 16, 32, x, y, 100, 200)
+        elif updown == -1:
+            character.clip_draw(0, 96, 16, 32, x, y, 100, 200)
+        elif dir == 1:
+            character.clip_draw(0, 64, 16, 32, x, y, 100, 200)
+        elif dir == -1:
+            character.clip_draw(0, 0, 16, 32, x, y, 100, 200)
+        else:
+            character.clip_draw(0, 96, 16, 32, x, y, 100, 200)
+    update_canvas()
+    frame = (frame + 1) % 4
+    x += dir * 10
+    y += updown *10
+    delay(0.1)
 
-        def draw(self):
-            if self.face_dir == 1:
-                self.image.clip_draw(self.frame * 16, 32, 16, 16, self.x, self.y)
+close_canvas()
