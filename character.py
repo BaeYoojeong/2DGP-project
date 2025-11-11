53b2945fa0a3e499c0151d48246fe22b43a35a53
iriring <juju5064@naver.com>
2025-11-12 02:00:28 +0900

    전 코드 키보드 입력에서 딜레이가 많이 걸려서 시간 기반 코드로 수정함

--- character.py
+++ character.py
@@ -1,5 +1,18 @@
 from pico2d import *
 
+# 속도
+PIXEL_PER_METER = (10.0 / 0.3)   # 10 pixel = 30 cm
+RUN_SPEED_KMPH = 10.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)  # 초당 픽셀 이동 속도
+
+# 애니메이션
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 4
+
+
 class Arang:
     def __init__(self):
         self.image = load_image('arang.png')
@@ -32,25 +45,30 @@ class Arang:
             elif event.key == SDLK_DOWN:
                 self.updown += 1
 
-    def update(self):
+    def update(self, frame_time):
+        # 이동 거리 계산
+        distance = RUN_SPEED_PPS * frame_time
+        # 애니메이션 프레임 업데이트
+        self.frame = (self.frame +
+                      FRAMES_PER_ACTION * ACTION_PER_TIME * frame_time) % FRAMES_PER_ACTION
+
         if self.dir != 0 or self.updown != 0:
-            self.frame = (self.frame + 1) % 4
-            self.x += self.dir * 10
-            self.y += self.updown * 10
+            self.x += self.dir * distance
+            self.y += self.updown * distance
             self.last_dir = self.dir
             self.last_updown = self.updown
-        else:
-            self.frame = 0
 
     def draw(self):
+        f = int(self.frame)
+
         if self.updown == 1:
-            self.image.clip_draw(self.frame * 16, 32, 16, 32, self.x, self.y, self.character_x, self.character_y)
+            self.image.clip_draw(f * 16, 32, 16, 32, self.x, self.y, self.character_x, self.character_y)
         elif self.updown == -1:
-            self.image.clip_draw(self.frame * 16, 96, 16, 32, self.x, self.y, self.character_x, self.character_y)
+            self.image.clip_draw(f * 16, 96, 16, 32, self.x, self.y, self.character_x, self.character_y)
         elif self.dir == 1:
-            self.image.clip_draw(self.frame * 16, 64, 16, 32, self.x, self.y, self.character_x, self.character_y)
+            self.image.clip_draw(f * 16, 64, 16, 32, self.x, self.y, self.character_x, self.character_y)
         elif self.dir == -1:
-            self.image.clip_draw(self.frame * 16, 0, 16, 32, self.x, self.y, self.character_x, self.character_y)
+            self.image.clip_draw(f * 16, 0, 16, 32, self.x, self.y, self.character_x, self.character_y)
         else:
             if self.last_updown == 1:
                 self.image.clip_draw(0, 32, 16, 32, self.x, self.y, self.character_x, self.character_y)
--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from pico2d import *
+import time
 import tile
 import tree
 import bush
 from character import Arang
 
+
 def main():
     open_canvas(800, 600)
+
     tile.load_tile_images()
     tree.load_tree_images()
     bush.load_bush_images()
+
     character = Arang()
 
     running = True
+    current_time = time.time()
+
     while running:
-        clear_canvas()
-        tile.draw_tile_map()
-        bush.draw_bush_map()
-        tree.draw_tree_map()
-        character.draw()
-
-        update_canvas()
-
+        new_time = time.time()
+        frame_time = new_time - current_time
+        current_time = new_time
 
         events = get_events()
         for e in events:
@@ -31,11 +32,18 @@ def main():
             else:
                 character.handle_event(e)
 
-        character.update()
-        delay(0.1)
+        character.update(frame_time)
+
+        clear_canvas()
+        tile.draw_tile_map()
+        bush.draw_bush_map()
+        character.draw()
+        tree.draw_tree_map()
+        update_canvas()
+
 
     close_canvas()
 
+
 if __name__ == '__main__':
     main()
-
