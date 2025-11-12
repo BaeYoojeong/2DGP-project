d2a66024b085ff3e729836a8d45b5ee64d384b16
iriring <juju5064@naver.com>
2025-11-12 10:25:14 +0900

    game frame work를 추가하고 main에 있던 작동 코드들을 play_mode로 이동, main에는 최소한의 코드만 남김

---
+++ play_mode.py
@@ -0,0 +1,75 @@
+from pico2d import *
+import game_world
+import time
+import tile
+import tree
+import bush
+from character import Arang
+
+
+character = None
+
+
+def init():
+    global character
+    open_canvas(800, 600)
+
+    tile.load_tile_images()
+    tree.load_tree_images()
+    bush.load_bush_images()
+    bush.create_bushes()
+
+    character = Arang()
+
+    for b in bush.bush_list:
+        game_world.add_collision_pair('arang:bush', character, b)
+
+
+def finish():
+    close_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for e in events:
+        if e.type == SDL_QUIT:
+            import game_framework
+            game_framework.quit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_ESCAPE:
+            import game_framework
+            game_framework.quit()
+        elif e.type == SDL_KEYDOWN and e.key == SDLK_h:
+            game_world.hitbox_draw()
+        else:
+            character.handle_event(e)
+
+
+def update():
+    static_prev = getattr(update, "_prev", None)
+    now = time.time()
+    if static_prev is None:
+        update._prev = now
+        frame_time = 0
+    else:
+        frame_time = now - static_prev
+        update._prev = now
+
+    game_world.handle_collisions()
+    character.update(frame_time)
+
+
+def draw():
+    clear_canvas()
+    tile.draw_tile_map()
+    bush.draw_bush_map()
+    character.draw()
+    tree.draw_tree_map()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
---
+++ start_frameWork.py
@@ -0,0 +1,35 @@
+from pico2d import *
+
+import game_framework
+import play_mode
+image = None
+
+def init():
+    # 타이틀 이미지를 로드
+    global image
+
+    image = load_image("title.png")
+
+def update():
+    pass
+
+def draw():
+    # 타이틀 이미지를 그려줌
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+def finish():
+    global image
+    del image
+
+def handle_events():
+    event_list = get_events()   # 현재까지 들어온 이벤트들을 받아온다.
+    # space키 이벤트 처리
+    for event in event_list:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
+            game_framework.change_mode(play_mode)
